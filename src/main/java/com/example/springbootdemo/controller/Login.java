a3e3224b63725c691753af0c82b597014875202e
Le-Tuan-2704 <tuan.ld184004@sis.hust.edu.vn>
2023-03-20 17:02:17 +0700

    start

---
+++ src/main/java/com/example/springbootdemo/controller/Login.java
@@ -0,0 +1,8 @@
+//package com.example.springbootdemo.controller;
+//
+//public class Login {
+//
+//    public String login(){
+//        return "a";
+//    }
+//}
---
+++ src/main/java/com/example/springbootdemo/model/User.java
@@ -0,0 +1,42 @@
+package com.example.springbootdemo.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String username;
+    private String password;
+    private String token;
+    private String userId;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
